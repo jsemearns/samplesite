b937778d52c62d2afc82efb2cc6eae647585065b
Justin <justin.mearns@gmail.com>
2019-01-21 17:48:46 +0800

    added templates, added related_name to models, modified views to use templates

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,8 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question,
-                on_delete=models.CASCADE)
+                on_delete=models.CASCADE,
+                related_name='choices')
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
